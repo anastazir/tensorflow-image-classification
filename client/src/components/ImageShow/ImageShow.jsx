626850519f5c13c8b961f4438817b1c287095891
Anas Tazir <atazir2018@gmail.com>
2021-09-18 22:55:22 +0530

    make new file to show image

---
+++ client/src/components/ImageShow/ImageShow.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+// eslint-disable-next-line jsx-a11y/img-redundant-alt
+import React from 'react'
+
+const ImageShow = () => {
+    const image='https://images.unsplash.com/photo-1554692918-08fa0fdc9db3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2MjI1Nn0'
+    return (
+        <div>
+            <div className="">
+        <img
+          src={image}
+          width="400"
+          crossOrigin="anonymous"
+          alt="Image"
+          
+        //   ref={imageRef}
+        />
+        {/* <div className="">
+
+          <button
+            className=""
+            onClick={() => console.log('clicking')}>sadfasdfasd
+          </button>
+        </div> */}
+        
+      </div>
+        </div>
+    )
+}
+
+export default ImageShow
