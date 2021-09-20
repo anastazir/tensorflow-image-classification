4975faf0494afc4fe2eb158bf4a6dfd45c7aed27
Anas Tazir <atazir2018@gmail.com>
2021-09-20 23:57:57 +0530

    add new router for base64 images

--- server/main.py
+++ server/main.py
@@ -1,7 +1,6 @@
 import flask
 import io
 import string
-import time
 import os
 import numpy as np
 from PIL import Image
@@ -16,6 +15,8 @@ from skimage import io
 app = Flask(__name__)
 
 model = tf.keras.models.load_model('masknet.h5')
+add='?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxmYWNlfGVufDB8fHx8MTYzMjA1MDM4MQ&ixlib=rb-1.2.1&q=80&w=400'
+face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def prepare_image(img):
     print('Preparing image')
@@ -59,93 +60,82 @@ def index():
     return 'Machine Learning Inference'
 
 
-# --------------------------------------
-from tensorflow.keras.preprocessing.image import  load_img
 
-def predict_image(filename):
-    img = load_img(filename, target_size=(128, 128))
-    image = tf.keras.preprocessing.image.img_to_array(img)
-    image = image / 255.0
-    image = image.reshape(1,128,128,3)
-    prediction = model.predict(image)
-    print ("prediction------",prediction[0])
-    if(prediction[0][0] > 0.5):
-        return 1
-    else:
-        return 0 
-
-
-# --------------------------------------
-@app.route("/send-image/<path:url>")
-def image_check(url):
-    from datetime import datetime
-    dateTimeObj = datetime.now()
-    file_name_for_base64_data = dateTimeObj.strftime("%d-%b-%Y--(%H-%M-%S)")
-    
-    file_name_for_regular_data = url[-10:-4]
-    print("url--------------------------",url)
-    try:
-        if "data:image/jpeg;base64," in url:
-            base_string = url.replace("data:image/jpeg;base64,", "")
-            decoded_img = base64.b64decode(base_string)
-            img = Image.open(BytesIO(decoded_img))
-
-            file_name = file_name_for_base64_data + ".jpg"
-            img.save(file_name, "jpeg")
-
-        elif "data:image/png;base64," in url:
-            base_string = url.replace("data:image/png;base64,", "")
-            decoded_img = base64.b64decode(base_string)
-            img = Image.open(BytesIO(decoded_img))
-
-            file_name = file_name_for_base64_data + ".png"
-            img.save(file_name, "png")
-
-        # Regular URL Form DATA
-        else:
-            print('sdafasdfasd')
-            response = requests.get(url)
-            img = Image.open(BytesIO(response.content)).convert("RGB")
-            img= img.resize((150, 150))
-            print(type(img))
-            file_name = file_name_for_regular_data + ".jpg"
-            img.save(file_name, "jpeg")
-            print('image saved')
-            prediction= predict_image(file_name)
-            return {"data": f"{prediction}"}
-
-        
-        status = "Image has been succesfully sent to the server."
-    except Exception as e:
-        status = "Error! = " + str(e)
-
-
-    return status
-
-@app.route("/url/<path:image_url>")
-def tensor(image_url):
-    print(image_url)
-    image_url = tf.keras.utils.get_file('Court', origin=image_url )
-    img = tf.keras.preprocessing.image.load_img(image_url, target_size=( 128, 128 ) )
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    print('--------------------------------final step------')
-    prediction = model.predict(np.expand_dims(img_array, axis=0)/255.0)
-    print ("prediction------",prediction[0])
-    if(prediction[0][0] > 0.5):
-        return {'data':'1'}
-    else:
-        return {'data':'0'}
-
-@app.route('/newRoute/<path:url>')
-def urlPred(url):
-    print('url is ', url)
+def urlPredSecondOption(url):  # IF NO FACES ARE FOUND
     sample_mask_img = io.imread(url)    
     sample_mask_img = cv2.resize(sample_mask_img,(128,128))
     sample_mask_img = np.reshape(sample_mask_img,[1,128,128,3])
     sample_mask_img = sample_mask_img/255.0
     pred= model.predict(sample_mask_img)
-    print('pred', pred)
-    if pred[0][0]>pred[0][1]:
+    print('NO FACE PRED---------------',pred)
+    if pred[0][1]<0.5:
+        print ('Mask')
+        return {'data':"Mask"}
+    else:
+        print ('No Mask')
+        return {'data':'No Mask'}
+
+
+# --------------------------------------
+@app.route("/send-image/<path:base64_string>")
+def decodeFace(base64_string): 
+    if "data:image/jpeg;base64," in base64_string:
+        base64_string = base64_string.replace("data:image/jpeg;base64,", "")
+    elif "data:image/png;base64," in base64_string:
+        base64_string = base64_string.replace("data:image/png;base64,", "")
+    elif "data:image/jpeg;base64," in base64_string:
+        base64_string = base64_string.replace("data:image/jpg;base64,", "")
+    if isinstance(base64_string, bytes):
+        base64_string = base64_string.decode("utf-8")
+
+    imgdata = base64.b64decode(base64_string)
+    img = io.imread(imgdata, plugin='imageio')
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+
+    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
+
+    for i in range(len(faces)):
+        (x,y,w,h) = faces[i]
+        crop = new_img[y:y+h,x:x+w]
+        crop = cv2.resize(crop,(128,128))
+        crop = np.reshape(crop,[1,128,128,3])/255.0
+        pred = model.predict(crop)
+        break
+    print('-----------------------pred[0]',pred[0])
+    if pred[0][1]<0.5:
+        print ('Mask')
+        return {'data':"Mask"}
+    else:
+        print ('No Mask')
+        return {'data':'No Mask'}
+
+
+@app.route('/newRoute/<path:url>')
+def urlPred(url):
+    if "https://images.unsplash.com/" in url:
+        url= url+ add 
+    print(url)
+    img = io.imread(url)    
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+
+    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
+    if len(faces)==0:
+        print('no faces were found')
+        return  urlPredSecondOption(url)
+    for i in range(len(faces)):
+        print('------------------found face')
+        (x,y,w,h) = faces[i]
+        crop = new_img[y:y+h,x:x+w]
+        crop = cv2.resize(crop,(128,128))
+        crop = np.reshape(crop,[1,128,128,3])/255.0
+        pred = model.predict(crop)
+        print('---------------------pred[0]',pred[0])
+        break                   # only predict for the first face
+    if pred[0][1]<0.5:
         print ('Mask')
         return {'data':"Mask"}
     else:
