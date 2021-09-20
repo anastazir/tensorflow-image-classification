e5e38a1a08becc88309f08d333324e754251139b
Anas Tazir <atazir2018@gmail.com>
2021-09-20 19:59:14 +0530

    add new fetch route

--- client/src/Fetch/fetchByUrl.js
+++ client/src/Fetch/fetchByUrl.js
@@ -1,18 +1,22 @@
 export const handleUrl = (text, setNotifications, add, style, notifications) =>{
-    fetch(`newRoute/${text}`).then((response) =>{ //make predictions
-      
-      console.log(response);
-      if(response.ok){
-        console.log('Ok');
-          return response.json()
-      }
+  fetch(`newRoute/${text}`).then((response) =>{ //make predictions
+    // console.log(response);
+    if(response.ok){
+      console.log('Ok');
+      return response.json()
+    }
   }).then(data =>{ 
       console.log("data---",data.data); // save the predictions in ans 
-      // setNotifications(add(notifications, text, style, ans))
       return data.data
     }).then(data =>{
       console.log("data----------",data);
       const ans = data;
       setNotifications(add(notifications, text, style, ans))
     })
-  }
+}
+
+export const handleBase64= (fileBase64String, setNotifications, add, style, notifications) =>{
+  fetch(`send-image/${fileBase64String}`).then((response) =>{
+    console.log('imageSent');
+  })
+}
