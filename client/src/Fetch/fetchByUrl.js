0f41f9161d4c146fec664db583e50b7ee5ae1c65
Anas Tazir <atazir2018@gmail.com>
2021-09-21 19:49:38 +0530

    change route locations

--- client/src/Fetch/fetchByUrl.js
+++ client/src/Fetch/fetchByUrl.js
@@ -1,25 +1,23 @@
 import { switchAns } from "../helper/helperFunctions";
 
-
 export const handleUrl = (text, setNotifications, add, style, notifications) =>{
-  fetch(`newRoute/${text}`).then((response) =>{ //make predictions
-    // console.log(response);
+  fetch(`${style}/urlRoute/${text}`).then((response) =>{ //make predictions
     if(response.ok){
       console.log('Ok');
       return response.json()
     }
   }).then(data =>{ 
-      console.log("data---",data.data); // save the predictions in ans 
+      console.log("data---",data.data); 
       return data.data
     }).then(data =>{
       console.log("data----------",data);
-      const ans = switchAns(data);
+      const ans = switchAns(data);  // save the predictions in ans 
       setNotifications(add(notifications, text, style, ans))
     })
 }
 
 export const handleBase64= (fileBase64String, setNotifications, add, style, notifications, text) =>{
-  fetch(`send-image/${fileBase64String}`).then((response) =>{
+  fetch(`${style}/base64/${fileBase64String}`).then((response) =>{
     console.log(fileBase64String);
     console.log('imageSent');
     return response.json()
@@ -28,7 +26,7 @@ export const handleBase64= (fileBase64String, setNotifications, add, style, noti
     return data.data
   }).then (data =>{
     console.log(data);
-    const ans = data;
+    const ans = switchAns(data);  // save the predictions in ans 
     setNotifications(add(notifications, text, style, ans))
   })
 }
