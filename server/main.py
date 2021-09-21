6220d323410c66adca5964c6dcca44a5d68d4b76
Anas Tazir <atazir2018@gmail.com>
2021-09-21 21:02:32 +0530

    add new routes for gender classificaton

--- server/main.py
+++ server/main.py
@@ -14,42 +14,13 @@ import cv2
 from skimage import io
 app = Flask(__name__)
 
-masknet = tf.keras.models.load_model('masknet.h5')
+masknet = tf.keras.models.load_model('masknet.h5') # input shape of (128, 128, 3)
+genderModel = tf.keras.models.load_model('GenderModal.h5') # input shape of (150, 150, 3)
 add='?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxmYWNlfGVufDB8fHx8MTYzMjA1MDM4MQ&ixlib=rb-1.2.1&q=80&w=400'
 face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-def prepare_image(img):
-    print('Preparing image')
-    img = Image.open(io.BytesIO(img))
-    img = img.resize((128, 128))
-    img = np.array(img)
-    img = np.expand_dims(img, 0)
-    print('returning img')
-    return img
-def predict_result(img):
-    print('Predict results')
-    predictions= masknet.predict(img)[0][0]
-    print(predictions)
-    if predictions>0.5:
-        return 1    # NO MASK
-    else:
-        return 0    #MASK
-@app.route('/predict', methods=['POST'])
-def infer_image():
-    if 'file' not in request.files:
-        return "Please try again. The Image doesn't exist"
-    print(request.files)
-    file = request.files.get('file')
-    if not file:
-        return
-    img_bytes = file.read()
-    img = prepare_image(img_bytes)
-    print('final step')
-    return jsonify(data=predict_result(img))
-@app.route('/', methods=['GET'])
-def index():
-    return 'Machine Learning Inference'
 
+# -------------------------------------- MASK CLASSIFICATION ------------------------------
 
 
 def urlPredSecondOption(url):  # IF NO FACES ARE FOUND
@@ -67,7 +38,7 @@ def urlPredSecondOption(url):  # IF NO FACES ARE FOUND
         return {'data':'No Mask'}
 
 
-# --------------------------------------
+
 @app.route("/faceMaskClassification/base64/<path:base64_string>")
 def decodeFace(base64_string): 
     if "data:image/jpeg;base64," in base64_string:
@@ -134,6 +105,72 @@ def urlPred(url):
         print ('No Mask')
         return {'data':'No Mask'}
 
+# -------------------------------------END OF MASK CLASSIFICATION ------------------------------
+
+@app.route('/genderClassification/urlRoute/<path:url>')
+def urlPredGenderClassification(url):
+    if "https://images.unsplash.com/" in url:
+        url= url+ add 
+    print(url)
+    img = io.imread(url)    
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+
+    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
+    # if len(faces)==0:
+    #     print('no faces were found')
+    #     return  urlPredSecondOption(url)
+    for i in range(len(faces)):
+        print('------------------found face')
+        (x,y,w,h) = faces[i]
+        crop = new_img[y:y+h,x:x+w]
+        crop = cv2.resize(crop,(150,150))
+        crop = np.reshape(crop,[1,150,150,3])/255.0
+        pred = genderModel.predict(crop)
+        print('---------------------pred[0]',pred[0])
+        break                   # only predict for the first face
+    if pred[0][1]<0.5:
+        print ('Female')
+        return {'data':"Female"}
+    else:
+        print ('Male')
+        return {'data':'Male'}
+
+@app.route("/genderClassification/base64/<path:base64_string>")
+def decodeGender(base64_string): 
+    if "data:image/jpeg;base64," in base64_string:
+        base64_string = base64_string.replace("data:image/jpeg;base64,", "")
+    elif "data:image/png;base64," in base64_string:
+        base64_string = base64_string.replace("data:image/png;base64,", "")
+    elif "data:image/jpeg;base64," in base64_string:
+        base64_string = base64_string.replace("data:image/jpg;base64,", "")
+    if isinstance(base64_string, bytes):
+        base64_string = base64_string.decode("utf-8")
+
+    imgdata = base64.b64decode(base64_string)
+    img = io.imread(imgdata, plugin='imageio')
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+
+    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
+    if len(faces)==0:
+        print('----------------------no faces were found')
+    for i in range(len(faces)):
+        (x,y,w,h) = faces[i]
+        crop = new_img[y:y+h,x:x+w]
+        crop = cv2.resize(crop,(150,150))
+        crop = np.reshape(crop,[1,150,150,3])/255.0
+        pred = genderModel.predict(crop)
+        break
+    print('-----------------------pred[0]',pred[0])
+    if pred[0][1]<0.5:
+        print ('Female')
+        return {'data':"Female"}
+    else:
+        print ('Male')
+        return {'data':'Male'}
 
 
 if __name__ == '__main__':
