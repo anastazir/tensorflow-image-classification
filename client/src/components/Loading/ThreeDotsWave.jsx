9976bb85f51997ecbb277d21996ea85675ae4281
Anas Tazir <atazir2018@gmail.com>
2021-09-22 12:20:48 +0530

    add a loading div

---
+++ client/src/components/Loading/ThreeDotsWave.jsx
@@ -0,0 +1,76 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const LoadingDot = {
+  display: "block",
+  width: "1.5rem",
+  height: "1.5rem",
+//   color: "#ffaa00",
+  backgroundColor: "#ffaa00",
+  borderRadius: "50%"
+};
+
+const LoadingContainer = {
+  width: "15rem",
+  height: "3rem",
+  display: "flex",
+  justifyContent: "space-around"
+};
+
+const ContainerVariants = {
+  initial: {
+    transition: {
+      staggerChildren: 0.2
+    }
+  },
+  animate: {
+    transition: {
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const DotVariants = {
+  initial: {
+    y: "0%"
+  },
+  animate: {
+    y: "100%"
+  }
+};
+
+const DotTransition = {
+  duration: 0.5,
+  yoyo: Infinity,
+  ease: "easeInOut"
+};
+
+export default function ThreeDotsWave() {
+  return (
+    <div className="loading"
+    >
+      <motion.div
+        style={LoadingContainer}
+        variants={ContainerVariants}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.span
+          style={LoadingDot}
+          variants={DotVariants}
+          transition={DotTransition}
+        />
+        <motion.span
+          style={LoadingDot}
+          variants={DotVariants}
+          transition={DotTransition}
+        />
+        <motion.span
+          style={LoadingDot}
+          variants={DotVariants}
+          transition={DotTransition}
+        />
+      </motion.div>
+    </div>
+  );
+}
