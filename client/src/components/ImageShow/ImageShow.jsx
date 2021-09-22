9d140cb49c1bf42cd3171d69f7c3174bddd003a2
Anas Tazir <atazir2018@gmail.com>
2021-09-22 20:48:32 +0530

    change url

--- client/src/components/ImageShow/ImageShow.jsx
+++ client/src/components/ImageShow/ImageShow.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const ImageShow = ({img}) => {
 
-    const image='https://images.unsplash.com/photo-1554692918-08fa0fdc9db3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2MjI1Nn0'
+    const image='https://images.unsplash.com/photo-1554692918-08fa0fdc9db3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxjb3JnaXxlbnwwfHx8fDE2MzIzMDA1OTg&ixlib=rb-1.2.1&q=80&w=1080'
     return (
         <div>
             <div className="">
