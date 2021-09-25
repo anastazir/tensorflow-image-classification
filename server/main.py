2e1fe4346e7d0e996a3ff289be8eae8376fbabfc
Anas Tazir <atazir2018@gmail.com>
2021-09-25 15:24:16 +0530

    import an use the find_face function

--- server/main.py
+++ server/main.py
@@ -12,6 +12,9 @@ from io import BytesIO
 import requests
 import cv2
 from skimage import io
+from faceMaskClassification import find_face
+
+
 app = Flask(__name__)
 
 masknet = tf.keras.models.load_model('masknet.h5') # input shape of (128, 128, 3)
@@ -20,7 +23,7 @@ catVsDogModel = tf.keras.models.load_model('catVsDogModel.h5') # input shape of 
 emotionClassification = tf.keras.models.load_model('emotionDetection.h5') # input shape of (48, 48, 1)   
 
 
-add='?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxmYWNlfGVufDB8fHx8MTYzMjA1MDM4MQ&ixlib=rb-1.2.1&q=80&w=150'
+add='?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxmYWNlfGVufDB8fHx8MTYzMjA1MDM4MQ&ixlib=rb-1.2.1&q=80&w=300'
 face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
@@ -57,7 +60,7 @@ def decodeFace(base64_string):
     img = io.imread(imgdata, plugin='imageio')
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=1) #returns a list of (x,y,w,h) tuples
 
     new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
     if len(faces)==0:
@@ -84,29 +87,7 @@ def urlPred(url):
         url= url+ add 
     print(url)
     img = io.imread(url)    
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
-
-    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
-    if len(faces)==0:
-        print('no faces were found')
-        return  urlPredSecondOption(new_img, shape=128)
-    for i in range(len(faces)):
-        print('------------------found face')
-        (x,y,w,h) = faces[i]
-        crop = new_img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(128,128))
-        crop = np.reshape(crop,[1,128,128,3])/255.0
-        pred = masknet.predict(crop)
-        print('---------------------pred[0]',pred[0])
-        break                   # only predict for the first face
-    if pred[0][1]<0.5:
-        print ('Mask')
-        return {'data':"Mask"}
-    else:
-        print ('No Mask')
-        return {'data':'No Mask'}
+    return find_face(img)
 
 # -------------------------------------END OF MASK CLASSIFICATION ------------------------------
 
@@ -134,13 +115,12 @@ def urlPredGenderClassification(url):
         crop = np.reshape(crop,[1,150,150,3])/255.0
         pred = genderModel.predict(crop)
         print('---------------------pred[0]',pred[0])
-        break                   # only predict for the first face
-    if pred[0][1]<0.5:
-        print ('Female')
-        return {'data':"Female"}
-    else:
-        print ('Male')
-        return {'data':'Male'}
+        if pred[0][1]<0.5:
+            print ('Female')
+            return {'data':"Female"}
+        else:
+            print ('Male')
+            return {'data':'Male'}
 
 @app.route("/genderClassification/base64/<path:base64_string>")
 def decodeGender(base64_string): 
@@ -236,7 +216,7 @@ def decodeAnimal(base64_string):
 def urlEmotionClassification(url):
     emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
     if "https://images.unsplash.com/" in url:
-        url= url+ add 
+        url= url+ add
     print(url)
     img = io.imread(url)    
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -275,8 +255,9 @@ def everything(url):
 
     faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
 
+    if len(faces)==0:return{'data': ['No face found']}
+
     for i in range(len(faces)):  # for emotion classification'
-        print('------------------found face')
         (x,y,w,h) = faces[i]
         crop = img[y:y+h,x:x+w]
         crop = cv2.resize(crop,(48,48))
@@ -284,12 +265,11 @@ def everything(url):
         pred = emotionClassification.predict(crop)
         print('---------------------pred[0]',pred)
         ans.append(emotion_labels[pred.argmax()])
-        break
         
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
-
+    faces = face_model.detectMultiScale(img,scaleFactor=1.06, minNeighbors=3) #returns a list of (x,y,w,h) tuples
+    print('------------------no of face', len(faces))
     for i in range(len(faces)):
         (x,y,w,h) = faces[i]
         crop = img[y:y+h,x:x+w]
@@ -303,7 +283,6 @@ def everything(url):
         ans.append('Male')
 
     for i in range(len(faces)):
-        print('------------------found face')
         (x,y,w,h) = faces[i]
         crop = img[y:y+h,x:x+w]
         crop = cv2.resize(crop,(128,128))
@@ -315,8 +294,7 @@ def everything(url):
     else:
         ans.append("No Mask")
 
-
-    return {"data":  ans}
+    return{'data': ans}
     
 # -------------------------------------END OF Everything CLASSIFICATION ------------------------------
 
