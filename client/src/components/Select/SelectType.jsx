e922eb90ef67bbdbc2b438ee16f1bc39b68f5177
Anas Tazir <atazir2018@gmail.com>
2021-10-04 21:34:51 +0530

    make a new input option to select the type of classification

---
+++ client/src/components/Select/SelectType.jsx
@@ -0,0 +1,16 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const SelectType = ({handleType}) => {
+    return (
+        <>
+        <motion.select className="input" onChange={handleType}>
+          <option value="showAll">Show All</option>
+          <option value="binaryClassification">Binary Classification</option>
+          <option value="multiclassClassification">Multiclass Classification</option>
+        </motion.select>  
+        </>
+    )
+}
+
+export default SelectType
