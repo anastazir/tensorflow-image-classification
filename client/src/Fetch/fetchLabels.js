f438e5caaebfb91af8b55a8f4ff129731b8fef10
Anas Tazir <atazir2018@gmail.com>
2021-10-07 20:03:45 +0530

    set labels from local storage if it exists

--- client/src/Fetch/fetchLabels.js
+++ client/src/Fetch/fetchLabels.js
@@ -1,13 +1,27 @@
+import { getLocalStorage } from "../helper/localStorage";
+const labelArray=[]
 
 export const getLabel = async (style, setLabels) => {
-    console.log('predicting');
+    let alreadyExists= getLocalStorage(style) 
+    if (alreadyExists) {
+      let savedResult= localStorage.getItem('myLabels')
+      savedResult= JSON.parse(savedResult)
+        for (let i=0; i<savedResult.length; i++) {
+        if(savedResult[i].labelName===style){
+          setLabels(savedResult[i].resultArray)
+          return 
+        }
+      }
+    }
     fetch(`fetchLabels?labelsType=${style}`).then((response) =>{ //make predictions
         if(response.ok){
-          console.log('Ok');
-         return response.json()
+          return response.json()
         }
       }).then(data =>{ 
-        // console.log("data---",data.labels); 
         setLabels(data.labels)
+        let currentLabel={'labelName': style,'resultArray': data.labels}
+        labelArray.push(currentLabel)
+        localStorage.setItem('myLabels', JSON.stringify(labelArray))
+        getLocalStorage(style)
       })
 }
--- client/src/helper/localStorage.js
+++ client/src/helper/localStorage.js
@@ -1,7 +1,11 @@
 export const getLocalStorage=(style)=>{
     let savedResult= localStorage.getItem('myLabels')
     savedResult= JSON.parse(savedResult)
+    if(!savedResult) return false
     for (let i=0; i<savedResult.length; i++) {
-        console.log(savedResult[i]);
+        if(savedResult[i].labelName===style){
+            return true;
+        }
     }
+    return false;
 }
