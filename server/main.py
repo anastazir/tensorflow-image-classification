2d3540e6e031f48515dc3992550b3961cd6367b0
Anas Tazir <atazir2018@gmail.com>
2021-10-12 13:14:30 +0530

    use the new route for classification and delete the old one.

--- server/main.py
+++ server/main.py
@@ -26,6 +26,7 @@ from foodClassification import foodClassificationURL
 from dogClassification import dogClassificationURL
 from birdsClassification import birdsClassificationURL
 from wildlifeClassification import wildlifeClassificationURL
+from everything import everythingURL
 from helperFunctions.returnArray import returnArray
 from fetchLabels import getLabels
 app = Flask(__name__)
@@ -83,7 +84,7 @@ def dynamicRoute(classificationType, url):
         return wildlifeClassificationURL(img)
 
     elif classificationType == "everything":
-        return everything(url)
+        return everythingURL(img)
 
     else:
         return {'data': 'this route does not exist'}
@@ -104,76 +105,6 @@ def uploadImageAndClassify(classificationType):
 
     else:
         return {'data': 'this route does not exist'}
-
-
-
-def everything(url):
-    ans=[]
-    emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
-    if "https://images.unsplash.com/" in url:
-        url= url+ add 
-    print(url)
-    img = io.imread(url)    
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
-    print('------------------no of face', len(faces))
-
-    if len(faces)==0:return{'data': ['No face found']}
-
-    for i in range(len(faces)):  # for emotion classification'
-        (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(48,48))
-        crop = np.reshape(crop,[1,48,48,1])/255.0
-        pred = emotionClassification.predict(crop)
-        print('---------------------pred[0]',pred)
-        ans.append(emotion_labels[pred.argmax()])
-        break
-        
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    # faces = face_model.detectMultiScale(img,scaleFactor=1.06, minNeighbors=3) #returns a list of (x,y,w,h) tuples
-    for i in range(len(faces)):
-        (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(150,150))
-        crop = np.reshape(crop,[1,150,150,3])/255.0
-        pred = genderModel.predict(crop)
-        break
-    if pred[0][1]<0.5:
-        ans.append('Female')
-    else:
-        ans.append('Male')
-
-    for i in range(len(faces)):
-        (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(128,128))
-        crop = np.reshape(crop,[1,128,128,3])/255.0
-        pred = masknet.predict(crop)
-        break                   # only predict for the first face
-    if pred[0][1]<0.5:
-        ans.append('Mask')
-    else:
-        ans.append("No Mask")
-
-    for i in range(len(faces)):
-        print('------------------found face')
-        (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(160,160))
-        crop = np.reshape(crop,[1,160,160,3])
-        pred = glassesModel.predict(crop)
-        print('---------------------pred[0]',pred[0])
-        # break                   # only predict for the first face
-        if pred[0][0]>0:
-            ans.append("No Glasses")
-        else:
-            ans.append('Glasses')
-        break
-    return{'data': ans}
-    
 
 
 if __name__ == '__main__':
