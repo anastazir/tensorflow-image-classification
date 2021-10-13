e0ea77167f3e74956df10db33611f4d19de060d5
Anas Tazir <atazir2018@gmail.com>
2021-10-13 12:23:55 +0530

    add age classification in the everthing file

--- server/everything.py
+++ server/everything.py
@@ -10,21 +10,24 @@ masknet = tf.keras.models.load_model('./models/masknet.h5') # input shape of (12
 genderModel = tf.keras.models.load_model('./models/GenderModal.h5') # input shape of (150, 150, 3)
 emotionClassification = tf.keras.models.load_model('./models/emotionDetection.h5') # input shape of (48, 48, 1)   
 glassesModel = tf.keras.models.load_model('./models/glassesDetection.h5') # input shape of (160, 160, 3)
+ageClassifier = tf.keras.models.load_model('./models/ageXception80.h5') # input shape of (80, 80, 1)   
 
 
 def everythingURL(img):
     ans=[]
     emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    age_labels = ['25-30','42-48','60-98','6-20']
 
-    faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_model.detectMultiScale(imgGray,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
     print('------------------no of face', len(faces))
 
     if len(faces)==0:return{'data': ['No face found']}
 
     for i in range(len(faces)):  # for emotion classification'
         (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
+        crop = imgGray[y:y+h,x:x+w]
         crop = cv2.resize(crop,(48,48))
         crop = np.reshape(crop,[1,48,48,1])/255.0
         pred = emotionClassification.predict(crop)
@@ -32,7 +35,6 @@ def everythingURL(img):
         ans.append(emotion_labels[pred.argmax()])
         break
         
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
     for i in range(len(faces)):
         (x,y,w,h) = faces[i]
@@ -71,5 +73,15 @@ def everythingURL(img):
             ans.append("No Glasses")
         else:
             ans.append('Glasses')
+        break
+    for i in range(len(faces)):
+        print('------------------found face')
+        (x,y,w,h) = faces[i]
+        crop = img[y:y+h,x:x+w]
+        crop = cv2.resize(crop,(80,80))
+        crop = np.reshape(crop,[1,80,80,3])/255.0
+        pred = ageClassifier.predict(crop)
+        print('---------------------pred[0]',pred)
+        ans.append(age_labels[pred.argmax()])
         break
     return{'data': ans}
