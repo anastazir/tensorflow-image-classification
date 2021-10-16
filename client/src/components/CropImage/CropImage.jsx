cd647ddd1c63d111a168aca85ec828595e044c73
Anas Tazir <atazir2018@gmail.com>
2021-10-16 12:53:41 +0530

    add new component to crop image form url

---
+++ client/src/components/CropImage/CropImage.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useCallback, useRef, useEffect } from "react";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+
+export default function CropImage({url}) {
+  console.log(url);
+  const [upImg, setUpImg] = useState(`${url}`);
+  const imgRef = useRef(null);
+  const previewCanvasRef = useRef(null);
+  const [crop, setCrop] = useState({ unit: "%", width: 30 });
+  const [completedCrop, setCompletedCrop] = useState(null);
+
+  const onLoad = useCallback((img) => {
+    imgRef.current = img;
+  }, []);
+
+  useEffect(() => {
+    if (!completedCrop || !previewCanvasRef.current || !imgRef.current) {
+      return;
+    }
+
+    const image = imgRef.current;
+    const canvas = previewCanvasRef.current;
+    const crop = completedCrop;
+
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    const ctx = canvas.getContext("2d");
+    const pixelRatio = window.devicePixelRatio;
+
+    canvas.width = crop.width * pixelRatio * scaleX;
+    canvas.height = crop.height * pixelRatio * scaleY;
+
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    ctx.imageSmoothingQuality = "high";
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width * scaleX,
+      crop.height * scaleY
+    );
+    console.log("dx is --", crop.x * scaleX);
+    console.log("dy is --", crop.y * scaleY);
+    console.log("dWidth is --", crop.width * scaleX);
+    console.log("dHeight is --", crop.height * scaleY);
+  }, [completedCrop]);
+
+  return (
+    <>
+        <div>
+          <ReactCrop
+            src={upImg}
+            onImageLoaded={onLoad}
+            crop={crop}
+            onChange={(c) => setCrop(c)}
+            onComplete={(c) => setCompletedCrop(c)}
+            crossOrigin="anonymous"
+          />
+        {/* </div>
+        <div> */}
+            <canvas
+            ref={previewCanvasRef}
+            style={{
+                width: Math.round(completedCrop?.width ?? 0),
+                height: Math.round(completedCrop?.height ?? 0)
+            }}
+            />
+        </div>
+    </>
+  );
+}
