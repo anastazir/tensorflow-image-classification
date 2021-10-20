d7c9f5afaa2af1e0a254ea76911d226b9719ec55
Anas Tazir <atazir2018@gmail.com>
2021-10-20 21:31:50 +0530

    add class name

--- client/src/components/ImageShow/ImageShow.jsx
+++ client/src/components/ImageShow/ImageShow.jsx
@@ -6,27 +6,14 @@ const ImageShow = ({img}) => {
 
     const image='https://images.unsplash.com/photo-1554692918-08fa0fdc9db3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxjb3JnaXxlbnwwfHx8fDE2MzIzMDA1OTg&ixlib=rb-1.2.1&q=80&w=1080'
     return (
-        <div>
-            <div className="">
-        <img
-        style={{borderRadius: "6px"}}
-          src={img ? img :  image}
-        //   width="400"
-          crossOrigin="anonymous"
-          alt="Image"
-          className="image-shown"
-          
-        //   ref={imageRef}
-        />
-        {/* <div className="">
-
-          <button
-            className=""
-            onClick={() => console.log('clicking')}>sadfasdfasd
-          </button>
-        </div> */}
-        
-      </div>
+        <div className="image-container">
+          <img
+          style={{borderRadius: "6px"}}
+            src={img ? img :  image}
+            crossOrigin="anonymous"
+            alt="Image"
+            className="image-shown"
+          />
         </div>
     )
 }
