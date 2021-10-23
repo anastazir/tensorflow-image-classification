191e342bcf7c41cd9f11f0e78eacdecf2c582dd2
Anas Tazir <atazir2018@gmail.com>
2021-10-23 23:29:33 +0530

    use tflite models instead of the original models

--- server/classifications/birdsClassification.py
+++ server/classifications/birdsClassification.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 from constants import birds_names_labels
-from loadModels import birdsClassification
+from loadModels import birdsClassification, birdsInterpreter
 
 def birdsClassificationURL(img):
     """
@@ -11,10 +11,16 @@ def birdsClassificationURL(img):
     img(numpy array) -- The array of the image to predict on.
     Return:The top three predictions in a JSON fromat.
     """
+    img_size= 224
+    input_details = birdsInterpreter.get_input_details()
+    output_details = birdsInterpreter.get_output_details()
     ans= []
-    new_img = cv2.resize(img,(224, 224))
-    new_img = np.reshape(new_img,[1, 224, 224, 3])/225.0
-    pred = birdsClassification.predict(new_img)
+    new_img = cv2.resize(img,(img_size, img_size)).astype("float32")
+    new_img = np.reshape(new_img,[1, img_size, img_size, 3])/225.0
+    birdsInterpreter.allocate_tensors()
+    birdsInterpreter.set_tensor(input_details[0]['index'], new_img)
+    birdsInterpreter.invoke()
+    pred = birdsInterpreter.get_tensor(output_details[0]['index'])
     arr= pred[0]
     arr1=arr.argsort()[-3:][::-1]  #get the top three results
     for i in arr1:
