79b4baf05b68ac6ed349dd62e450675a255ccb57
Anas Tazir <atazir2018@gmail.com>
2021-10-27 19:23:24 +0530

    use .tflite models instead of the .h5

--- server/classifications/ageClassification.py
+++ server/classifications/ageClassification.py
@@ -1,27 +1,33 @@
 import cv2
-import tensorflow as tf
 import numpy as np
 
 
 face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-from loadModels import ageClassifier
+from loadModels import ageInterpreter
 
 def ageClassificationURL(img):
-    age_labels = ['18-30','40-60','60-98','6-18']
+    age_labels = ['0-5', '19-30', '31-50', '51-65', '6-18', '66-100']
 
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_model.detectMultiScale(imgGray,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
+    faces = face_model.detectMultiScale(img_gray,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
 
-    print("number of faces are----",len(faces))
-    for i in range(len(faces)):
-        print('------------------found face')
-        (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(80,80))
-        crop = np.reshape(crop,[1,80,80,3])/255.0
-        pred = ageClassifier.predict(crop)
-        print('---------------------pred[0]',pred)
-        return {"data":  f"{age_labels[pred.argmax()]}"}
-    return {"data": 'Face not found'}
+    img_shape= 160
+    if len(faces)==0: return {"data": "No faces were found"}
+    print('------------------found face')
+    (x,y,w,h) = faces[0]
+    new_img = img[y:y+h,x:x+w]
+    input_details = ageInterpreter.get_input_details()
+    output_details = ageInterpreter.get_output_details()
+    new_img = cv2.resize(img,(img_shape, img_shape)).astype("float32")
+    new_img = np.reshape(new_img,[1, img_shape, img_shape, 3])/255.0
+
+
+    ageInterpreter.allocate_tensors()
+    ageInterpreter.set_tensor(input_details[0]['index'], new_img)
+    ageInterpreter.invoke()
+    pred = ageInterpreter.get_tensor(output_details[0]['index'])
+
+    print('---------------------pred[0]',pred[0])
+    return  {"data":  f"{age_labels[pred.argmax()]}"}
--- server/classifications/emotionClassification.py
+++ server/classifications/emotionClassification.py
@@ -2,29 +2,31 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
-from loadModels import emotionClassification
+from loadModels import emotionClassification, emotionInterpreter
 
 face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def emotionClassificationURL(img):
     emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
-
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4) #returns a list of (x,y,w,h) tuples
 
-    # new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
-    # if len(faces)==0:
-    #     print('no faces were found')
-    #     return  urlPredSecondOption(new_img, shape=128)
-    print("number of faces are----",len(faces))
-    for i in range(len(faces)):
-        print('------------------found face')
-        (x,y,w,h) = faces[i]
-        crop = img[y:y+h,x:x+w]
-        crop = cv2.resize(crop,(48,48))
-        crop = np.reshape(crop,[1,48,48,1])/255.0
-        pred = emotionClassification.predict(crop)
-        print('---------------------pred[0]',pred)
-        return {"data":  f"{emotion_labels[pred.argmax()]}"}
-    return {"data": 'Face not found'}
+    img_shape= 48
+    if len(faces)==0: return {"data": "No faces were found"}
+    print('------------------found face')
+    (x,y,w,h) = faces[0]
+    new_img = img[y:y+h,x:x+w]
+    input_details = emotionInterpreter.get_input_details()
+    output_details = emotionInterpreter.get_output_details()
+    new_img = cv2.resize(img,(img_shape, img_shape)).astype("float32")
+    new_img = np.reshape(new_img,[1, img_shape, img_shape, 1])/255.0
+
+
+    emotionInterpreter.allocate_tensors()
+    emotionInterpreter.set_tensor(input_details[0]['index'], new_img)
+    emotionInterpreter.invoke()
+    pred = emotionInterpreter.get_tensor(output_details[0]['index'])
+
+    print('---------------------pred[0]',pred[0])
+    return  {"data":  f"{emotion_labels[pred.argmax()]}"}
