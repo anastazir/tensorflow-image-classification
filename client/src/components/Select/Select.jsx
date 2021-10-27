65984406f99d158c2889ecd9aabbcc3ec9d315da
Anas Tazir <atazir2018@gmail.com>
2021-10-27 19:18:46 +0530

    add logic for InputList component

--- client/src/components/Select/Select.jsx
+++ client/src/components/Select/Select.jsx
@@ -2,15 +2,18 @@ import { motion } from 'framer-motion'
 import React from 'react'
 import {optionValues} from '../../helper/optionValues'
 
-const InputList= optionValues.map((option, i)=>
-    (<option value={option.value} key={i}>{option.name}</option>)
-)
+const InputList=({category})=> optionValues.map((option, i)=>{
+    if (option.type.includes(category)===true){
+        return (<option value={option.value} key={i}>{option.name}</option>)
+    }
+})
 
-const Select = ({handleStyle}) => {
+const Select = ({handleStyle, category}) => {
+    console.log("category in Select.jsx is" ,category);
     return (  
         <>
         <motion.select className="input" onChange={handleStyle}>
-          {InputList}
+          <InputList category={category} />
         </motion.select>  
         </>
     )
