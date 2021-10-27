1366dd9af61c5a22950c310ea1857b2384c49d16
Anas Tazir <atazir2018@gmail.com>
2021-10-27 19:16:23 +0530

    add new type

--- client/src/helper/optionValues.js
+++ client/src/helper/optionValues.js
@@ -2,61 +2,61 @@ export const optionValues=[
     {
         name: '🔥 Classify Everything',
         value: 'everything',
-        type: ['faceClassification']
+        type: ['faceClassification', 'showAll']
     },
     {
         name: '🤿 Mask Classification',
         value: 'faceMaskClassification',
-        type: ['binaryClassification', 'faceClassification']
+        type: ['binaryClassification', 'faceClassification', 'showAll']
     },
     {
         name: '♂️ or ♀️ Classification',
         value: 'genderClassification',
-        type: ['binaryClassification', 'faceClassification']
+        type: ['binaryClassification', 'faceClassification', 'showAll']
     },
     {
         name: '👨‍🦰 Emotion Detection',
         value: 'emotionClassification',
-        type: ['multiClassification', 'faceClassification']
+        type: ['multiClassification', 'faceClassification', 'showAll']
     },
     {
         name: '👓or👀',
         value: 'glassesClassification',
-        type: ['binaryClassification', 'faceClassification']
+        type: ['binaryClassification', 'faceClassification', 'showAll']
     },
     {
         name: '🧔 Age Classification',
         value: 'ageClassification',
-        type: ['multiClassification', 'faceClassification']
+        type: ['multiClassification', 'faceClassification', 'showAll']
     },
     {
         name: '🐱or🐶',
         value: 'catvsDog',
-        type: ['binaryClassification', 'animalClassification']
+        type: ['binaryClassification', 'animalClassification', 'showAll']
     },
     {
         name: '🍲 Food Classification',
         value: 'foodClassification',
-        type: ['multiClassification']
+        type: ['multiClassification', 'showAll']
     },
     {
         name: '🐕‍🦺 Dog Classification',
         value: 'dogClassification',
-        type: ['multiClassification', 'animalClassification']
+        type: ['multiClassification', 'animalClassification', 'showAll']
     },
     {
         name: '🦅 Birds Classification',
         value: 'birdsClassification',
-        type: ['multiClassification', 'animalClassification']
+        type: ['multiClassification', 'animalClassification', 'showAll']
     },
     {
         name: '🦙 Wildlife Classification',
         value: 'wildlifeClassification',
-        type: ['multiClassification', 'animalClassification']
+        type: ['multiClassification', 'animalClassification', 'showAll']
     },
     {
         name: '🌻 Flower Classification',
         value: 'flowerClassification',
-        type: ['multiClassification']
+        type: ['multiClassification', 'showAll']
     },
 ]
