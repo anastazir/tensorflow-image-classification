c41730fecb7d6c0bea7e2fe1ceb1f0e5f737bf24
Anas Tazir <atazir2018@gmail.com>
2021-10-29 21:37:25 +0530

    make a post request instead of get and edit urlRoute

--- client/src/Fetch/fetchByUrl.js
+++ client/src/Fetch/fetchByUrl.js
@@ -1,39 +1,35 @@
 import { switchAns } from "../helper/helperFunctions";
 let newImage=null
+
 export const handleUrl = (text, setNotifications, add, style, notifications, setPredicting, openResultdModal) =>{
-  setPredicting(true)
-  fetch(`${style}/urlRoute/${text}`).then((response) =>{ //make predictions
-    if(response.ok){
-      return response.json()
-    }
-  })
-  .catch((error) =>{
-    console.log("error-->", error);
+  
+  const formData = new FormData();
+  formData.append('url', text)
+  const Upload = async() => {
+    setPredicting(true)
+    await fetch(`/urlRoute/${style}`, {
+      method: 'POST',
+      body: formData
+    }).then(response => {
+        response.json().then(data => {
+        if (Array.isArray(data.data)){
+          openResultdModal(data.data)
+        }else{
+        const ans = switchAns(data.data);
+        let text=''
+        setNotifications(add(notifications,text, style, ans))
+        }
+      })
+    })
     setPredicting(false)
-  })
-  .then(data =>{ 
-      console.log("data---",data.data); 
-      return data.data
-    })
-    .catch((error) =>{
-      console.log("error in second catch is-->", error);
-    })
-    .then(data =>{
-      if (Array.isArray(data)){
-        console.log('array found');
-        openResultdModal(data)
-      }else{
-        const ans = switchAns(data);  // save the predictions in ans 
-        setNotifications(add(notifications, text, style, ans))
-      }
-      setPredicting(false)
-    })
+  }
+  Upload();
+
 }
 
 
-
-
 export const handleUpload=(uploadedImage, style, add, notifications, setPredicting, openResultdModal, setNotifications) =>{
+  
   setPredicting(true)
   const formData = new FormData();
   newImage= uploadedImage ? uploadedImage : newImage
@@ -79,7 +75,6 @@ export const handleCroppedImage =(text, coordinates, style, add, notifications, 
         setPredicting(false)
         if (Array.isArray(data.data)){
           openResultdModal(data.data)
-          setPredicting(false)
         }else{
         const ans = switchAns(data.data);
         let text=''
@@ -87,7 +82,7 @@ export const handleCroppedImage =(text, coordinates, style, add, notifications, 
         }
       })
     })
+    setPredicting(false)
   }
   Upload();
-  
 }
