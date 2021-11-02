43c4c39355a74b42823b8e306567408131c9e0d1
Anas Tazir <atazir2018@gmail.com>
2021-11-02 16:17:31 +0530

    crop numpy array if coordinates are given

--- server/helperFunctions/returnArray.py
+++ server/helperFunctions/returnArray.py
@@ -10,4 +10,11 @@ def returnArray(request):
     if image:
         img= Image.open(image)
         numpydata = asarray(img)
-        return numpydata
+    if request.form["isCropped"] == 'true':
+        dx= int(request.form['dx'])
+        dy= int(request.form['dy'])
+        dHeight= int(request.form['dHeight'])
+        dWidth= int(request.form['dWidth'])
+        print(dx, dy, dHeight, dWidth)
+        numpydata= numpydata[dy:dy+dHeight, dx:dx+dWidth]
+    return numpydata
