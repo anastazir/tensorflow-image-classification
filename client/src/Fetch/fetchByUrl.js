0760618da8f48b53081e8f00876fa6073702c2a3
Anas Tazir <atazir2018@gmail.com>
2021-11-03 19:55:27 +0530

    convert the image file into base 64 string

--- client/src/Fetch/fetchByUrl.js
+++ client/src/Fetch/fetchByUrl.js
@@ -1,3 +1,4 @@
+import Compress from 'compress.js';
 import { switchAns } from "../helper/helperFunctions";
 let newImage=null
 
@@ -37,36 +38,45 @@ export const handleUrl = (text, coordinates, setNotifications, add, style, notif
 
 
 export const handleUpload=(uploadedImage, coordinates, style, add, notifications, setPredicting, openResultdModal, setNotifications, isCrop) =>{
-  
-  var dx, dy, dHeight, dWidth;
-  [dx, dy, dHeight, dWidth]= coordinates
+  const compress= new Compress()
+  var [dx, dy, dHeight, dWidth]= coordinates
   newImage= uploadedImage ? uploadedImage : newImage
+  console.log(newImage)
+  compress.compress([newImage], {
+    size: 4, 
+    quality: .75,
+    maxWidth: 1920, 
+    maxHeight: 1920, 
+    resize: true,
+    rotate: false,
+  }).then((data) => {
 
-  const formData = new FormData();
-  formData.append('isCropped', !isCrop)
-  formData.append('dx', Math.floor(dx));
-  formData.append('dy', Math.floor(dy));
-  formData.append('dHeight', Math.floor(dHeight));
-  formData.append('dWidth', Math.floor(dWidth));
-  formData.append('file', newImage);
+    const formData = new FormData();
+    formData.append('isCropped', !isCrop)
+    formData.append('dx', Math.floor(dx));
+    formData.append('dy', Math.floor(dy));
+    formData.append('dHeight', Math.floor(dHeight));
+    formData.append('dWidth', Math.floor(dWidth));
+    formData.append('base64', data[0].data);
 
-  const Upload = async() => {
-    setPredicting(true)
-    await fetch(`upload-image/${style}`, {
-      method: 'POST',
-      body: formData
-    }).then(response => {
-        response.json().then(data => {
-        if (Array.isArray(data.data)){
-          openResultdModal(data.data)
-        }else{
-        const ans = switchAns(data.data);
-        let text=''
-        setNotifications(add(notifications,text, style, ans))
-        }
+    const Upload = async() => {
+      setPredicting(true)
+      await fetch(`upload-image/${style}`, {
+        method: 'POST',
+        body: formData
+      }).then(response => {
+          response.json().then(data => {
+          if (Array.isArray(data.data)){
+            openResultdModal(data.data)
+          }else{
+          const ans = switchAns(data.data);
+          let text=''
+          setNotifications(add(notifications,text, style, ans))
+          }
+        })
       })
-    })
-    setPredicting(false)
-  }
-  Upload();
+      setPredicting(false)
+    }
+    Upload();
+  })
 }
