0a3da88dd8955e9ccdaf5149ee7567819ec909f0
Anas Tazir <atazir2018@gmail.com>
2021-11-03 19:53:27 +0530

    convert the base64 image to a numpy array

--- server/helperFunctions/returnArray.py
+++ server/helperFunctions/returnArray.py
@@ -1,20 +1,22 @@
 from PIL import Image
 from numpy import asarray
+from PIL import Image
+from io import BytesIO
+import base64
 
 def returnArray(request):
     """
-    Takes the request as a parameter, then finds the file attached to it and
+    Takes the request as a parameter, then finds the base64 string attached to it and
     converts it into a numpy array.
     """
-    image = request.files["file"]
-    if image:
-        img= Image.open(image)
-        numpydata = asarray(img)
+    if request.form["base64"]:
+        base64str= request.form["base64"]
+        imageDecoded = Image.open(BytesIO(base64.b64decode(base64str)))
+        numpydata = asarray(imageDecoded)
     if request.form["isCropped"] == 'true':
         dx= int(request.form['dx'])
         dy= int(request.form['dy'])
         dHeight= int(request.form['dHeight'])
         dWidth= int(request.form['dWidth'])
-        print(dx, dy, dHeight, dWidth)
         numpydata= numpydata[dy:dy+dHeight, dx:dx+dWidth]
     return numpydata
