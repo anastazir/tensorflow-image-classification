c9e1ec6d333dccc097d50f1d23bf83954bf57afa
Anas Tazir <atazir2018@gmail.com>
2021-11-04 22:21:11 +0530

    add try-catch code

--- client/src/Fetch/fetchLabels.js
+++ client/src/Fetch/fetchLabels.js
@@ -2,26 +2,28 @@ import { getLocalStorage } from "../helper/localStorage";
 const labelArray=[]
 
 export const getLabel = async (style, setLabels) => {
-    let alreadyExists= getLocalStorage(style) 
-    if (alreadyExists) {
-      let savedResult= localStorage.getItem('myLabels')
-      savedResult= JSON.parse(savedResult)
-        for (let i=0; i<savedResult.length; i++) {
-        if(savedResult[i].labelName===style){
-          setLabels(savedResult[i].resultArray)
-          return 
-        }
+  let alreadyExists= getLocalStorage(style) 
+  if (alreadyExists) {
+    let savedResult= localStorage.getItem('myLabels')
+    savedResult= JSON.parse(savedResult)
+      for (let i=0; i<savedResult.length; i++) {
+      if(savedResult[i].labelName===style){
+        setLabels(savedResult[i].resultArray)
+        return 
       }
     }
-    fetch(`fetchLabels?labelsType=${style}`).then((response) =>{ //make predictions
-        if(response.ok){
-          return response.json()
-        }
-      }).then(data =>{ 
-        setLabels(data.labels)
-        let currentLabel={'labelName': style,'resultArray': data.labels}
-        labelArray.push(currentLabel)
-        localStorage.setItem('myLabels', JSON.stringify(labelArray))
-        getLocalStorage(style)
-      })
+  }
+  fetch(`fetchLabels?labelsType=${style}`).then((response) =>{ //make predictions
+  if(response.ok){
+    return response.json()
+  }
+  }).then(data =>{ 
+    setLabels(data.labels)
+    let currentLabel={'labelName': style,'resultArray': data.labels}
+    labelArray.push(currentLabel)
+    localStorage.setItem('myLabels', JSON.stringify(labelArray))
+    getLocalStorage(style)
+  }).catch((error) =>{
+    setLabels(['Loading..'])
+  })
 }
