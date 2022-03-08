da48e72d6d44731bf529868d885da74dd847eb27
Anas Tazir <atazir2018@gmail.com>
2022-03-09 01:30:25 +0530

    add a button with a github icon

--- client/src/App.js
+++ client/src/App.js
@@ -213,6 +213,9 @@ const Header = () => (
     <motion.h1 className="pink">
       Tensorflow
       <span className="light-blue"> Project</span>
+      <button style={{float: "right", backgroundColor: "black"}}>
+        <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" />
+      </button>
     </motion.h1>
   </div>
 );
