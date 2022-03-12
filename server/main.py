f87f9a9c2270e71d0c4efb117bbab71ddf5eb32e
Anas Tazir <atazir2018@gmail.com>
2022-03-12 21:52:27 +0530

    use new classes

--- server/main.py
+++ server/main.py
@@ -1,25 +1,15 @@
 import io
 from flask import Flask, request
 from skimage import io
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+
 
 # IMPORT FUNCTIONS
-from classifications.everything             import everythingURL
-from classifications.faceMaskClassification import maskClassification
-from classifications.genderClassification   import genderClassification
-from classifications.emotionClassification  import emotionClassificationURL
-from classifications.glassesClassification  import glassesClassificationURL
-from classifications.ageClassification      import ageClassificationURL
-from classifications.catOrDog               import catOrDogClassification
-from classifications.dogClassification      import dogClassificationURL
-from classifications.birdsClassification    import birdsClassificationURL
-from classifications.wildlifeClassification import wildlifeClassificationURL
-from classifications.foodClassification     import foodClassificationURL
-from classifications.flowerClassification   import flowerClassificationURL
-from classifications.animalClassification   import animalsClassificationURL
-from classifications.landmarkClassification import landmarkClassificationURL
-from helperFunctions.returnArray            import returnArray
-from fetchLabels                            import getLabels
+from helperFunctions.returnArray      import returnArray
+from fetchLabels                      import getLabels
+from classifications.EverythingClass  import EverythingClassification
+from classifications.SingleClassifier import MultiClassifier
+from classifications.MultiClassifier  import SingleClassifier
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -31,7 +21,6 @@ def sendLabels():
     print('Labeltype is ---------', labelType)
     result= getLabels(labelType)
     return {'labels': result}
-            
 
 @app.route('/urlRoute/<classificationType>', methods=['POST'])
 def dynamicRoute(classificationType):
@@ -41,66 +30,30 @@ def dynamicRoute(classificationType):
     url= request.form['url']
 
     try:
-        img = io.imread(url)  
+        img = io.imread(url)
 
     except:
         return {'data': 'ERROR: unable to read image'}
 
     if request.form["isCropped"] == 'true':
-        isCropped= True
+        isCropped = True
         dx= int(request.form['dx'])
         dy= int(request.form['dy'])
         dHeight= int(request.form['dHeight'])
         dWidth= int(request.form['dWidth'])
         img= img[dy:dy+dHeight, dx:dx+dWidth]
-        
-    else:
-        isCropped= False
-
-    if classificationType == "catvsDog":
-        return catOrDogClassification(img)
-
-    elif classificationType == "faceMaskClassification":
-        return maskClassification(img, isCropped=isCropped)
-
-    elif classificationType == "genderClassification":
-        return genderClassification(img, isCropped=isCropped)
-
-    elif classificationType == "emotionClassification":
-        return emotionClassificationURL(img, isCropped=isCropped)
-
-    elif classificationType == "glassesClassification":
-        return glassesClassificationURL(img, isCropped=isCropped)
-
-    elif classificationType == "foodClassification":
-        return foodClassificationURL(img)
-
-    elif classificationType == "dogClassification":
-        return dogClassificationURL(img)
-
-    elif classificationType == "birdsClassification":
-        return birdsClassificationURL(img)
-
-    elif classificationType == "wildlifeClassification":
-        return wildlifeClassificationURL(img)
-
-    elif classificationType == "landmarkClassification":
-        return landmarkClassificationURL(img)
-
-    elif classificationType == "everything":
-        return everythingURL(img, isCropped=isCropped)
-
-    elif classificationType == "ageClassification":
-        return ageClassificationURL(img, isCropped=isCropped)
-
-    elif classificationType == "flowerClassification":
-        return flowerClassificationURL(img)
-
-    elif classificationType == "animalClassification":
-        return animalsClassificationURL(img)
 
     else:
-        return {'data': 'this route does not exist'}
+        isCropped = False
+
+    if classificationType == "everything":
+        classifier = EverythingClassification(isCropped = isCropped)
+    elif classificationType in ["faceMaskClassification", "genderClassification", "emotionClassification", "glassesClassification", "ageClassification", "catvsDog"]:
+        classifier = MultiClassifier(type= classificationType, isCropped = isCropped)
+    else:
+        classifier = SingleClassifier(type = classificationType)
+
+    return classifier.perdict_image(img)
 
 @app.route('/upload-image/<classificationType>', methods=['POST'])
 def uploadImageAndClassify(classificationType):
@@ -111,50 +64,13 @@ def uploadImageAndClassify(classificationType):
         isCropped= True
     else: isCropped= False
 
-    if classificationType == "catvsDog":
-        return catOrDogClassification(returnArray(request))
+    img = returnArray(request)
 
-    elif classificationType == "faceMaskClassification":
-        return maskClassification(returnArray(request), isCropped= isCropped)
-
-    elif classificationType == "genderClassification":
-        return genderClassification(returnArray(request), isCropped= isCropped)
-
-    elif classificationType == "landmarkClassification":
-        return landmarkClassificationURL(returnArray(request))
-
-    elif classificationType == "emotionClassification":
-        return emotionClassificationURL(returnArray(request), isCropped= isCropped)
-
-    elif classificationType == "glassesClassification":
-        return glassesClassificationURL(returnArray(request), isCropped= isCropped)
-
-    elif classificationType == "foodClassification":
-        return foodClassificationURL(returnArray(request))
-
-    elif classificationType == "dogClassification":
-        return dogClassificationURL(returnArray(request))
-
-    elif classificationType == "birdsClassification":
-        return birdsClassificationURL(returnArray(request))
-
-    elif classificationType == "wildlifeClassification":
-        return wildlifeClassificationURL(returnArray(request))
-
-    elif classificationType == "everything":
-        return everythingURL(returnArray(request), isCropped= isCropped)
-
-    elif classificationType == "ageClassification":
-        return ageClassificationURL(returnArray(request), isCropped= isCropped)
-
-    elif classificationType == "flowerClassification":
-        return  flowerClassificationURL(returnArray(request))
-
-    elif classificationType == "animalClassification":
-        return animalsClassificationURL(returnArray(request))
-
+    if classificationType in ["faceMaskClassification", "genderClassification", "emotionClassification", "glassesClassification", "ageClassification", "catvsDog"]:
+        classifier = SingleClassifier(type= classificationType, isCropped = isCropped)
     else:
-        return {'data': 'this route does not exist'}
+        classifier = MultiClassifier(type = classificationType)
+    return classifier.perdict_image(img)
 
 @app.route('/')
 def ping():
