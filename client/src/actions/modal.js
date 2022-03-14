0def06452348d73cb82c1523b180fea8f8893dc2
Anas Tazir <atazir2018@gmail.com>
2022-03-14 23:02:31 +0530

    update notification list

--- client/src/actions/modal.js
+++ client/src/actions/modal.js
@@ -1,32 +1,21 @@
-import { START_LOADING, END_LOADING, RESULT, ERROR, OPEN } from '../constants/modalTypes';
+import { START_LOADING, END_LOADING, RESULT, ERROR, OPEN, ADD } from '../constants/modalTypes';
 import * as api from '../api/index.js';
-
-export const startLoading = () => (dispatch) => {
-    console.log('================================actins , startLoading', START_LOADING);
-    dispatch({type: START_LOADING});
-}
-
-export const endLoading = () => (dispatch) => {
-    console.log('================================actins , endLoading');
-    dispatch({type: END_LOADING});
-}
+import { switchAns } from '../helper/helperFunctions';
 
 export const predictImage = (formData, style) => async (dispatch) => {
     dispatch({type: START_LOADING});
     try {
         const { data: { data } } = await api.predict_image(formData, style);
         dispatch({ type: RESULT, data });
-        console.log("data type ", Array.isArray(data))
         if (Array.isArray(data)){
             dispatch({ type: OPEN });
         }else{
-            
+            const ans = {
+                style : style,
+                ans : switchAns(data)
+            }
+            dispatch({ type: ADD, data : ans });
         }
-        //   else{
-        //   const ans = switchAns(data.data);
-        //   let text=''
-        //   setNotifications(add(notifications,text, style, ans))
-        //   }
     } catch (error) {
         dispatch({ type: ERROR, error });
     }
