01be3c7414ba64a511b2c4959da5a50ce62e82fe
Anas Tazir <atazir2018@gmail.com>
2022-03-15 02:29:36 +0530

    add new api to predict on local files

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,5 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://127.0.0.1:5000' });
 
 export const predict_image = (formData, style) => API.post(`/urlRoute/${style}`, formData);
+
+export const predict_file = (fromData, style) => API.post(`/upload-image/${style}`, fromData)
