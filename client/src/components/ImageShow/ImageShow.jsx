4a648a2888fba2ea461913d310c38b6afbdbe8a5
Anas Tazir <atazir2018@gmail.com>
2022-03-17 23:41:14 +0530

    import image URL/image file useDispatch

--- client/src/components/ImageShow/ImageShow.jsx
+++ client/src/components/ImageShow/ImageShow.jsx
@@ -1,21 +1,23 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 // eslint-disable-next-line jsx-a11y/img-redundant-alt
-import React from 'react'
+import React from           'react'
+import { useSelector } from 'react-redux';
 
-const ImageShow = ({img}) => {
+const ImageShow = () => {
 
-    const image='https://images.unsplash.com/photo-1554692918-08fa0fdc9db3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTYyOTB8MHwxfHNlYXJjaHw5fHxjb3JnaXxlbnwwfHx8fDE2MzIzMDA1OTg&ixlib=rb-1.2.1&q=80&w=1080'
-    return (
-        <div className="image-container">
-          <img
-          style={{borderRadius: "6px"}}
-            src={img ? img :  image}
-            crossOrigin="anonymous"
-            alt="Image"
-            className="image-shown"
-          />
-        </div>
-    )
+  const image = useSelector((state) => state.imageReducer.image)
+
+  return (
+      <div className="image-container">
+        <img
+        style={{borderRadius: "6px"}}
+          src={image}
+          crossOrigin="anonymous"
+          alt="Image"
+          className="image-shown"
+        />
+      </div>
+  )
 }
 
 export default ImageShow
