91bb936be0f667b513c85228db276513126b985a
Anas Tazir <atazir2018@gmail.com>
2022-03-18 22:51:03 +0530

    only contain 10 notifications

--- client/src/reducers/notifications.js
+++ client/src/reducers/notifications.js
@@ -4,6 +4,9 @@ import { remove } from "../helper/arr-utils"
 const notificationReducer = (state = {notificationList: []}, action) => {
     switch (action.type) {
         case actionTypes.ADD:
+            if (state.notificationList.length >= 10){
+                state.notificationList.shift()
+            }
             return {...state, notificationList : [...state.notificationList, action.data]}
         case actionTypes.REMOVE:
             const updatedArray = remove(state.notificationList, action.data)
