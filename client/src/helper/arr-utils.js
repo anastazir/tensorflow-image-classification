2ac799402f2aab94ced66602ee686a6fe76e3875
Anas Tazir <atazir2018@gmail.com>
2022-03-21 23:28:15 +0530

    remove text parameter

--- client/src/helper/arr-utils.js
+++ client/src/helper/arr-utils.js
@@ -1,4 +1,3 @@
-// MacGuyver'd utility to generate && remove notifications
 export const remove = (arr, item) => {
   const newArr = [...arr];
   newArr.splice(
@@ -9,8 +8,7 @@ export const remove = (arr, item) => {
 };
 
 let newIndex = 0;
-export const add = (arr, text, style, ans) => {
+export const add = (arr, style, ans) => {
   newIndex = newIndex + 1;
-  console.log("ans in utils.js is",ans);
-  return [...arr, { id: newIndex, text: text, style: style, ans: ans }];
+  return [...arr, { id: newIndex, style: style, ans: ans }];
 };
