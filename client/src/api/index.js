b71fca8c87db2e688f7e05cc0e5e59f2dbfbbf30
Anas Tazir <atazir2018@gmail.com>
2022-03-22 23:25:33 +0530

    add api request to fetch labels from server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,4 +4,6 @@ const API = axios.create({ baseURL: 'http://127.0.0.1:5000' });
 
 export const predict_image = (formData, style) => API.post(`/urlRoute/${style}`, formData);
 
-export const predict_file = (fromData, style) => API.post(`/upload-image/${style}`, fromData)
+export const predict_file = (fromData, style) => API.post(`/upload-image/${style}`, fromData);
+
+export const fetch_labels = (style) => API.get(`/fetchLabels?labelsType=${style}`)
