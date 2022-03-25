a41da498317448f12b11a09a2b2eb82e1e460f5f
Anas Tazir <atazir2018@gmail.com>
2022-03-26 01:31:13 +0530

    add href link to github repository

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,12 +3,14 @@ import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <motion.h1 className="pink">
+    <motion.h1 style={{textAlign: 'center'}} className="pink">
         Tensorflow
-        <span className="light-blue"> Project</span>
-        <button style={{float: "right", backgroundColor: "black"}} onClick={() => window.open("https://github.com/anastazir/tensorflow-image-classification", "_blank")}>
+        <span className="light-blue"> Project </span>
+        <span>
+          <a target="_blank" style={{ backgroundColor: "black"}} href="https://github.com/anastazir/tensorflow-image-classification" rel="noreferrer">
             <img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white" />
-        </button>
+          </a>
+        </span>
     </motion.h1>
   )
 }
