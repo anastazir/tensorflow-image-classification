9a322bd272a5e961d6877238006accc97d3966c9
Anas Tazir <atazir2018@gmail.com>
2022-03-25 23:30:13 +0530

    show both result array and result string through ResultDiv

--- client/src/components/ResultDiv/index.jsx
+++ client/src/components/ResultDiv/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-const ResultDiv = ({ handleClose }) => {
+const ResultDiv = () => {
+    const dispatch = useDispatch()
+
     const data = useSelector((state) => state.modalReducer.result)
-    if (!data){
-        return <></>
+
+    const handleClick = () =>{
+        dispatch({type : "CLOSE"})
+     }
+
+     if (Array.isArray(data)){
+        return (
+            <div className="result-text" onClick={handleClick}>
+                {data.map((data, i)=>{
+                    return <li key={i} className="result-text-li">{data}</li>
+                })}
+            </div>
+        )
+    }else{
+        return (
+            <div className="result-text">
+                <li className="result-text-li">{data}</li>
+            </div>
+        )
     }
-    return (
-        <div className="result-text" onClick={handleClose}>
-            {data.map((data, i)=>{
-                return <li key={i} className="result-text-li">{data}</li>
-            })}
-        </div>
-    )
+
 }
 
 export default ResultDiv
