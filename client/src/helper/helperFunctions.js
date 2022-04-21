1703641a266f5db503555d69e4d29e8d916faec0
Anas Tazir <atazir2018@gmail.com>
2022-04-21 20:13:49 +0530

    remove encodeFileBase64 function

--- client/src/helper/helperFunctions.js
+++ client/src/helper/helperFunctions.js
@@ -1,19 +1,3 @@
-export   const encodeFileBase64 = (file, setFileBase64String) => {
-    var reader = new FileReader();
-    console.log(file);
-    if (file) {
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        var Base64 = reader.result;
-        // console.log(Base64);
-        setFileBase64String(Base64);
-      };
-      reader.onerror = (error) => {
-        console.log("error: ", error);
-      };
-    }
-};
-
 export const switchAns = (data) =>{
   if (data==='Mask'){
     data= data+' 😷'
