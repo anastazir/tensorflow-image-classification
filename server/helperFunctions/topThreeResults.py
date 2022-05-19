ec38a4ea8575b6e2c194946742c3a32a5dd98ddb
Anas Tazir <atazir2018@gmail.com>
2022-05-19 23:23:47 +0530

    add functionality to the topThreeResults function

--- server/helperFunctions/topThreeResults.py
+++ server/helperFunctions/topThreeResults.py
@@ -1,2 +1,6 @@
-def topThreeResults(array, labels):
-    pass
+def topThreeResults(pred, labels):
+    ans = []
+    arr= pred[0]
+    array=array.argsort()[-3:][::-1]
+    for i in array:
+        ans.append(f'{labels[i].title()}: {float("{0:.2f}".format(arr[i]))*100}%')
